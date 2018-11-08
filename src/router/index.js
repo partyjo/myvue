bc9b7104f9fb773d7a29c39b7d0e31005ff4f68b
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-11-08 14:10:34 +0800

    tets

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Clear from '@/components/Clear'
 import Help from '@/components/Help'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/help/:id',
       name: 'Help',
       component: Help
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
     }
   ]
 })
